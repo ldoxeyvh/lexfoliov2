c596b73e40fb49b7dcca704b88bedbb91cf3e0d7
Leif Doxey-Van Heest <ldoxeyvh@umich.edu>
2025-04-10 19:57:19 -0400

    added more real routes and finished about page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,34 +23,39 @@
 	let { children } = $props();
 </script>
 
-<div class="lg:p-5 p-3 bg-zinc-100">
+<div class="lg:p-5 p-3 bg-amber-50 min-h-screen">
     <div class="border-2 border-slate-800 lg:px-20 lg:py-12">
         <!-- desktop -->
-        <div class="hidden lg:grid grid-cols-2 gap-y-12 w-full h-8"> 
-            <!-- <a href="" class="col-span-2 justify-self-center" aria-label="logo/homepage link">
-                <img src={Logo} alt="logo" class="h-16"/>
-            </a> -->
-            <a href="{`${base}/`}" class="group">
-                <span class="text-xl lg:text-2xl xl:text-3xl text-slate-800 group-hover:font-semibold transition-all">ALEXSEY DACOSTA</span>
-                <div class="w-1/2 mt-4 border-b-2 border-slate-800 group-hover:w-2/3 transition-all"></div>
+        <div class="hidden lg:grid grid-cols-4 gap-y-12 w-full mb-20"> 
+            <!-- <div class="col-span-2">
+                <a href="" class="h-16 w-16 block" aria-label="logo/homepage link">
+                    <img src={Logo} alt="logo"/>
+                </a>
+            </div> -->
+            <a href="{`${base}/`}" class="flex gap-x-10 group">
+                <img src={Logo} alt="logo" class="w-16 h-16"/>
+                <div class="">
+                    <span class="text-xl lg:text-2xl xl:text-3xl text-slate-800 group-hover:font-semibold transition-all font-lexend">ALEXSEY DACOSTA</span>
+                    <div class="w-full mt-4 border-b-2 border-slate-800 group-hover:w-[150%] transition-all"></div>
+                </div>
             </a>
-            <div class="grid grid-cols-5 gap-x-4">
+            <div></div>
+            <div class="col-span-2 grid grid-cols-5 gap-x-4">
                 <a href="{`${base}/portfolio`}" class="group col-span-2">
-                    <span class="text-xl lg:text-2xl xl:text-3xl text-slate-800 group-hover:font-semibold transition-all">PORTFOLIO</span>
+                    <span class="text-xl lg:text-2xl xl:text-3xl text-slate-800 group-hover:font-semibold transition-all font-lexend">PORTFOLIO</span>
                     <div class="w-2/3 group-hover:w-full mt-4 border-b-2 border-slate-800 transition-all"></div>
                 </a>
                 <div class="grid grid-cols-2 gap-x-4 col-span-3">
-                    <a href="" class="group">
-                        <span class="text-xl lg:text-2xl xl:text-3xl text-slate-800 group-hover:font-semibold transition-all">CONTACT</span>
+                    <a href="{`${base}/contact`}" class="group">
+                        <span class="text-xl lg:text-2xl xl:text-3xl text-slate-800 group-hover:font-semibold transition-all font-lexend">CONTACT</span>
                         <div class="w-2/3 group-hover:w-full mt-4 border-b-2 border-slate-800 transition-all"></div>
                     </a>
-                    <a href="" class="group">
-                        <span class="text-xl lg:text-2xl xl:text-3xl text-slate-800 group-hover:font-semibold transition-all">ABOUT ME</span>
+                    <a href="{`${base}/about-me`}" class="group">
+                        <span class="text-xl lg:text-2xl xl:text-3xl text-slate-800 group-hover:font-semibold transition-all font-lexend whitespace-nowrap">ABOUT ME</span>
                         <div class="w-2/3 group-hover:w-full mt-4 border-b-2 border-slate-800 transition-all"></div>
                     </a>
                 </div>
             </div>
-            
         </div>
         <!-- mobile -->
         <div class="lg:hidden grid grid-cols-4 mx-2 mt-2 pb-2 border-b border-slate-800">
@@ -73,7 +78,7 @@
                 <a href="" class="mx-2 mt-2 pb-2  text-xl">TEST</a>
             </div>
         {/if}
-        <div class="lg:h-52"></div> <!-- filler -->
+        <!-- <div class="lg:h-52"></div> filler -->
         <!-- <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
 
         </div> -->
---
+++ src/routes/about-me/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Me from '$lib/assets/about-me/MEPICTURE.jpg'
+</script>
+
+<div class="grid grid-cols-2 justify-items-center gap-40">
+    <img src={Me} alt="me" class="border-2 border-slate-800 max-w-lg justify-self-end"/>
+    <p class="text-2xl place-self-center justify-self-start max-w-2xl">
+        Hi! My name is Alexsey daCosta and I am a Graphic Designer. I have a BA from the University of Michigan where I studied Art & Design.
+        <br><br>
+        I am passionate about the environment and all things creative. In my free time you can find me taking walks, birdwatching, or playing video games.
+        <br><br>
+        If you like what you see I would love to work with you! 
+    </p>
+</div>
