6dfda0cc88fe19ec5f8d1d3192e5afd1011b0bf5
Leif Doxey-Van Heest <ldoxeyvh@umich.edu>
2025-06-06 21:36:32 -0500

    json loading of image links on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,31 @@
 <script lang="ts">
+    import { base } from "$app/paths";
+    import elements from "$lib/assets/new-homepage/elements.json"
+    import { crossfade } from "svelte/transition";
+    
     const imageModules = import.meta.glob(
      '$lib/assets/new-homepage/*{.jpg,.png,.jpeg,.gif}',
         {
-            eager: true
+            eager: true,
+            query: '?url'
         }
     ) as Record<string, { default: string }>;
     const images = Object.values(imageModules).map(module => module.default);
+
+    const [send, receive] = crossfade({});
+
+    function filenameToUrl(target: string): string | undefined {
+        return images.find(path => {
+            const filename = path.split('/').pop();
+            return filename === target;
+        })
+    }
 </script>
 
 <div class="columns-1 lg:columns-2 gap-4 m-4">
-    {#each images as image}
-        <img class="w-full mb-4 border border-slate-500" src={image} alt="TODO" />
+    {#each elements as element}
+        <a href={`${base}/${element["dest"]}`} aria-label={element.alt}>
+            <img class="w-full mb-4 border hover:border-3 border-slate-800" src={filenameToUrl(element["main-img"])} alt={element.alt} />
+        </a> 
     {/each}
 </div>
